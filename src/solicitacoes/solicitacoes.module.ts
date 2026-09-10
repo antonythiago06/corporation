8e844d0e36de3f70afde82765d3a5a7a6077b84a
Thiago <antonythiagoaraujo@gmail.com>
2026-09-10 16:28:42 -0300

    feat: Adiciona import de Auditoria

--- src/solicitacoes/solicitacoes.module.ts
+++ src/solicitacoes/solicitacoes.module.ts
@@ -3,11 +3,12 @@ import { SolicitacoesController } from './solicitacoes.controller';
 import { SolicitacoesService } from './solicitacoes.service';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auditoria } from '../auditoria/auditoria.entity';
 import { Solicitacao } from './solicitacao.entity';
 
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([Solicitacao])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Solicitacao, Auditoria])],
   controllers: [SolicitacoesController],
   providers: [SolicitacoesService],
 })
